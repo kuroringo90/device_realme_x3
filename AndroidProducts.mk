c373567fb171d3d59115e7c42522cffde685da31
karthik1896 <karthik1896@outlook.com>
2022-06-09 23:56:16 +0530

    x3:Welcome to Styx Project

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_x3.mk
+    $(LOCAL_DIR)/styx_x3.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_x3-user \
-    aosp_x3-userdebug \
-    aosp_x3-eng
+    styx_x3-user \
+    styx_x3-userdebug \
+    styx_x3-eng
